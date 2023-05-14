c0af372e4b9b052e678a2883e1d61bb87361a586
vot3tada <zindromed@bk.ru>
2023-05-14 15:19:09 +0300

    made download repository by link to server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { spawn } = require('child_process');
+const {exec, spawn} = require('child_process');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
@@ -7,13 +8,23 @@ const port = 3000;
 app.get('/', (req, res) => {
     const {git} = req.query;
     if (git === undefined) return res.json({message: 'No data'})
-    const script = spawn('python', ['script.py', git]);
-    let data = '';
-    script.stdout.on('data', (chunk) => {
-        data += chunk;
-    });
-    script.on('close', () => {
-        res.json({message: data});
+    const repoUrl = git; // URL репозитория
+    let repoPath = './repositories/';
+    repoPath += fs.readdirSync(repoPath).length;
+    fs.mkdirSync(repoPath);
+    exec(`git clone ${repoUrl} ${repoPath}`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return res.json({message: 'Invalid link'});
+        }
+        const script = spawn('python', ['script.py', repoPath]);
+        let data = '';
+        script.stdout.on('data', (chunk) => {
+            data += chunk;
+        });
+        script.on('close', () => {
+            res.json({message: data});
+        });
     });
 });
 
